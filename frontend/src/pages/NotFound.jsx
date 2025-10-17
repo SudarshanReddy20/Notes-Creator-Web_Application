7f24bbf4d1ad8081d3ee2c4063ad7ca92b839892
Sudarshan Reddy Medapati <sudarshan382003@gmail.com>
2025-10-17 20:57:39 +0530

    Files Update

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The page you're looking for doesn't exist!</p>
+    </div>
   )
 }
 
