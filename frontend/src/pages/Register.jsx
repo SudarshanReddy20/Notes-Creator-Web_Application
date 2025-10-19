c0602079eebcfee7faed254f55d881ec7fcea728
Sudarshan Reddy Medapati <sudarshan382003@gmail.com>
2025-10-19 15:52:48 +0530

    Url issue

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,7 +3,7 @@ import Form from '../components/Form'
 
 function Register() {
   return (
-    <Form route="api/user/register/" method="register" />
+    <Form route="/api/user/register/" method="register" />
   )
 }
 
